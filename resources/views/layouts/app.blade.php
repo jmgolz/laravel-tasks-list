9bd844893cfa821ce6d851cda4ec432fa16a1bb7
James Golz <jmgolz@gmail.com>
2015-11-20 21:42:42 +0000

    add in more styles, app works. can add/delete tasks

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -38,10 +38,29 @@
     <div class="container">
         <nav class="navbar navbar-default">
             <!-- Navbar Contents -->
+            <nav class="navbar navbar-default">
+    			<div class="container-fluid ">
+    				<div class="navbar-header">
+    					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+    						<span class="sr-only">Toggle navigation</span>
+    						<span class="icon-bar"></span>
+    						<span class="icon-bar"></span>
+    						<span class="icon-bar"></span>
+    					</button>
+    
+    					<a class="navbar-brand" href="#">Task List</a>
+    				</div>
+    
+    				<div id="navbar" class="navbar-collapse collapse">
+    					<ul class="nav navbar-nav">
+    						&nbsp;
+    					</ul>
+    				</div>
+    			</div>
+    		</nav>
         </nav>
-    </div>
-
     @yield('content')
+    </div>
 </body>
 
 </html>
