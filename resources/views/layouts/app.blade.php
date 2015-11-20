1726a497d6d49a42856c7944734311574ef03f8e
James Golz <jmgolz@gmail.com>
2015-11-20 21:34:27 +0000

    add in more basics. need full styles

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,20 +1,47 @@
-// resources/views/layouts/app.blade.php
-
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <title>Laravel Quickstart - Basic</title>
 
-        <!-- CSS And JavaScript -->
-    </head>
+<head>
+    <title>Laravel Quickstart - Basic</title>
 
-    <body>
-        <div class="container">
-            <nav class="navbar navbar-default">
-                <!-- Navbar Contents -->
-            </nav>
-        </div>
+    <!-- CSS And JavaScript -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,500,700" rel="stylesheet" type="text/css">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet" type="text/css">
 
-        @yield('content')
-    </body>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0-alpha1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
+    <style>
+        body {
+            font-family: 'Raleway';
+            margin-top: 25px;
+        }
+        
+        button .fa {
+            margin-right: 6px;
+        }
+        
+        .table-text div {
+            padding-top: 6px;
+        }
+    </style>
+
+    <script>
+        $(function() {
+            $('#task-name').focus();
+        });
+    </script>
+</head>
+
+<body>
+    <div class="container">
+        <nav class="navbar navbar-default">
+            <!-- Navbar Contents -->
+        </nav>
+    </div>
+
+    @yield('content')
+</body>
+
 </html>
--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -1,5 +1,3 @@
-// resources/views/tasks.blade.php
-
 @extends('layouts.app')
 
 @section('content')
@@ -34,5 +32,40 @@
         </form>
     </div>
 
-    <!-- TODO: Current Tasks -->
+    <!-- Current Tasks -->
+    @if (count($tasks) > 0)
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                Current Tasks
+            </div>
+            
+            <div class="panel-body">
+                <table class="table table-striped task-table">
+                    <thead>
+                        <th>Task</th>
+                        <th>&nbsp;</th>
+                    </thead>
+                    
+                    <tbody>
+                        @foreach ($tasks as $task)
+                            <tr>
+                                <td class="table-text">
+                                    <div>{{ $task->name }}</div>
+                                </td>
+                                
+                                <td>
+                                    <form action="/task/{{ $task->id }}" method="POST">
+                                        {{ csrf_field() }}
+                                        {{ method_field('DELETE') }}
+                                        
+                                        <button>Delete Task</button>
+                                    </form>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    @endif
 @endsection
